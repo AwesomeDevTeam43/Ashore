421efe1df829a0ab7e2179916667db52d53f98d7
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2025-09-24 10:41:26 +0100

    Player Jump

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -4,9 +4,13 @@ using UnityEngine.InputSystem;
 
 public class Player_Controller : MonoBehaviour
 {
+    [SerializeField] public Vector2 moveInput;
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     public float moveSpeed = 5f;
-    public Vector2 moveInput;
-    public Rigidbody2D rb;
+    public float jumpingPower = 2f;
 
     public bool IsMoving { get; private set; }
 
@@ -25,10 +29,25 @@ public class Player_Controller : MonoBehaviour
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
     }
 
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+
+        
     }
 }
