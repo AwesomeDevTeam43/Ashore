65a6e3f6add0f609babc69ded912155b7c9544b7
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2025-09-25 14:44:08 +0100

    Look Around

--- Assets/Scripts/Player_Camera.cs
+++ Assets/Scripts/Player_Camera.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 public class Player_Camera : MonoBehaviour
 {
     [SerializeField] private Player_InputHandler player_InputHandler;
-
     [SerializeField] private CinemachineCamera cam;
+
+    [Header ("LookAround")]
     [SerializeField] private float maxOffset = 3f;
     [SerializeField] private float smooth = 5f;
 
+    [Header("CameraShake")]
+    [SerializeField] private float hitAmplitudeGain = 2f;
+    [SerializeField] private float hitFrequencyGain = 2f;
+
     private CinemachinePositionComposer positionComposer;
+    private CinemachineBasicMultiChannelPerlin noisePerlin;
     private Vector3 targetOffset;
 
     void Start()
     {
         positionComposer = cam.GetComponent<CinemachinePositionComposer>();
+        noisePerlin = cam.GetComponent<CinemachineBasicMultiChannelPerlin>();
     }
 
     void Update()
+    {
+        LookAround();
+    }
+
+    private void LookAround()
     {
         float camX = player_InputHandler.LookInput.x;
         float camY = player_InputHandler.LookInput.y;
@@ -27,4 +39,9 @@ public class Player_Camera : MonoBehaviour
         positionComposer.TargetOffset = Vector3.Lerp(positionComposer.TargetOffset, targetOffset, Time.deltaTime * smooth);
     }
 
+    public void CameraShake()
+    {
+        noisePerlin.AmplitudeGain = hitAmplitudeGain;
+        noisePerlin.FrequencyGain = hitFrequencyGain;
+    }
 }
