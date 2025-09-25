c2d6f2d4cc9748d80ab710b59ebb2139b95dcebf
João Manuel <148542897+Jo6o43@users.noreply.github.com>
2025-09-25 14:17:56 +0100

    Look Around

--- Assets/Scripts/Player_Camera.cs
+++ Assets/Scripts/Player_Camera.cs
@@ -1,16 +1,30 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Player_Camera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Player_InputHandler player_InputHandler;
+
+    [SerializeField] private CinemachineCamera cam;
+    [SerializeField] private float maxOffset = 3f;
+    [SerializeField] private float smooth = 5f;
+
+    private CinemachinePositionComposer positionComposer;
+    private Vector3 targetOffset;
+
     void Start()
     {
-        
+        positionComposer = cam.GetComponent<CinemachinePositionComposer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float camX = player_InputHandler.LookInput.x;
+        float camY = player_InputHandler.LookInput.y;
+
+        targetOffset = new Vector3(camX * maxOffset, camY * maxOffset, 0);
+
+        positionComposer.TargetOffset = Vector3.Lerp(positionComposer.TargetOffset, targetOffset, Time.deltaTime * smooth);
     }
+
 }
