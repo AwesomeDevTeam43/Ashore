7b114dfcf8389e646c5155006a43b50826bb44e8
Gonki04 <goncaloaraujo2004@gmail.com>
2025-10-23 15:30:44 +0100

    update enemy health

--- Assets/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Scripts/Enemy/Enemy_Health.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 public class Enemy_Health : MonoBehaviour
 {
     [SerializeField]
+    private int maxHealth;
+    [SerializeField]
     private bool damageable = true;
-    [SerializeField] 
-    private int maxHealth = 100;
     [SerializeField]
     private float invincibilityDuration = .2f;
     private bool hit;
-    private int currentHealth;
     private HealthSystem healthSystem;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentHealth = maxHealth;
         healthSystem = GetComponent<HealthSystem>();
+        healthSystem.OnHealthChanged += OnPlayerHealthChanged;
         healthSystem.Initialize(maxHealth);
     }
-    
+
     public void TakeDamage(int damage)
     {
-        if(damageable && !hit && currentHealth > 0)
+        if (damageable && !hit && healthSystem.CurrentHealth > 0)
         {
             hit = true;
-            currentHealth -= damage;
             healthSystem.TakeDamage(damage, gameObject);
-            if (currentHealth <= 0)
-            {
-                Debug.Log($"{gameObject.name} died.");
-                Destroy(gameObject);
-            }
-            else
-            {
-                Debug.Log($"{gameObject.name} took {damage} damage. Current health: {currentHealth}/{maxHealth}");
-                StartCoroutine(TurnOffHit());
-            }
+            OnPlayerHealthChanged(healthSystem.CurrentHealth, healthSystem.MaxHealth);
+        }
+    }
+
+    private void OnPlayerHealthChanged(int currentHealth, int maxHealth)
+    {
+        if (currentHealth <= 0)
+        {
+            Debug.Log($"{gameObject.name} has died.");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log($"{gameObject.name} took damage. Current health: {currentHealth}/{maxHealth}");
+            StartCoroutine(TurnOffHit());
         }
     }
 
